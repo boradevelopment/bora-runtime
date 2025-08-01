66a4e948d8ed58e03a956180a3594a5ab2ed7d79
wanomaniac <me@wanomani.ac>
2025-08-01 20:42:20 +0200

    add contribs/wasm-micro-runtime

---
+++ contribs/wasm-micro-runtime/core/iwasm/libraries/wasi-nn/include/wasi_nn.h
@@ -0,0 +1,92 @@
+/*
+ * Copyright (C) 2019 Intel Corporation.  All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+ */
+
+/**
+ * Following definition from:
+ * [Oct 25th, 2022]
+ * https://github.com/WebAssembly/wasi-nn/blob/0f77c48ec195748990ff67928a4b3eef5f16c2de/wasi-nn.wit.md
+ */
+
+#ifndef WASI_NN_H
+#define WASI_NN_H
+
+#include <stdint.h>
+#include "wasi_nn_types.h"
+
+#define WASI_NN_IMPORT(name) \
+    __attribute__((import_module("wasi_nn"), import_name(name)))
+
+/**
+ * @brief Load an opaque sequence of bytes to use for inference.
+ *
+ * @param builder   Model builder.
+ * @param encoding  Model encoding.
+ * @param target    Execution target.
+ * @param g         Graph.
+ * @return wasi_nn_error    Execution status.
+ */
+wasi_nn_error
+load(graph_builder_array *builder, graph_encoding encoding,
+     execution_target target, graph *g) WASI_NN_IMPORT("load");
+
+wasi_nn_error
+load_by_name(const char *name, uint32_t name_len, graph *g)
+    WASI_NN_IMPORT("load_by_name");
+
+/**
+ * INFERENCE
+ *
+ */
+
+/**
+ * @brief Create an execution instance of a loaded graph.
+ *
+ * @param g         Graph.
+ * @param ctx       Execution context.
+ * @return wasi_nn_error    Execution status.
+ */
+wasi_nn_error
+init_execution_context(graph g, graph_execution_context *ctx)
+    WASI_NN_IMPORT("init_execution_context");
+
+/**
+ * @brief Define the inputs to use for inference.
+ *
+ * @param ctx       Execution context.
+ * @param index     Input tensor index.
+ * @param tensor    Input tensor.
+ * @return wasi_nn_error    Execution status.
+ */
+wasi_nn_error
+set_input(graph_execution_context ctx, uint32_t index, tensor *tensor)
+    WASI_NN_IMPORT("set_input");
+
+/**
+ * @brief Compute the inference on the given inputs.
+ *
+ * @param ctx       Execution context.
+ * @return wasi_nn_error    Execution status.
+ */
+wasi_nn_error
+compute(graph_execution_context ctx) WASI_NN_IMPORT("compute");
+
+/**
+ * @brief Extract the outputs after inference.
+ *
+ * @param ctx                   Execution context.
+ * @param index                 Output tensor index.
+ * @param output_tensor         Buffer where output tensor with index `index` is
+ * copied.
+ * @param output_tensor_size    Pointer to `output_tensor` maximum size.
+ *                              After the function call it is updated with the
+ * copied number of bytes.
+ * @return wasi_nn_error                Execution status.
+ */
+wasi_nn_error
+get_output(graph_execution_context ctx, uint32_t index,
+           tensor_data output_tensor, uint32_t *output_tensor_size)
+    WASI_NN_IMPORT("get_output");
+
+#endif
---
+++ contribs/wasm-micro-runtime/core/iwasm/libraries/wasi-nn/include/wasi_nn_types.h
@@ -0,0 +1,138 @@
+/*
+ * Copyright (C) 2019 Intel Corporation.  All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+ */
+
+#ifndef WASI_NN_TYPES_H
+#define WASI_NN_TYPES_H
+
+#include <stdint.h>
+#include <stdbool.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * ERRORS
+ *
+ */
+
+// sync up with
+// https://github.com/WebAssembly/wasi-nn/blob/71320d95b8c6d43f9af7f44e18b1839db85d89b4/wasi-nn.witx#L5-L17
+// Error codes returned by functions in this API.
+typedef enum {
+    success = 0,
+    invalid_argument,
+    invalid_encoding,
+    missing_memory,
+    busy,
+    runtime_error,
+    unsupported_operation,
+    too_large,
+    not_found,
+
+    // for WasmEdge-wasi-nn
+    end_of_sequence = 100,  // End of Sequence Found.
+    context_full = 101,     // Context Full.
+    prompt_tool_long = 102, // Prompt Too Long.
+    model_not_found = 103,  // Model Not Found.
+} wasi_nn_error;
+
+/**
+ * TENSOR
+ *
+ */
+
+// The dimensions of a tensor.
+//
+// The array length matches the tensor rank and each element in the array
+// describes the size of each dimension.
+typedef struct {
+    uint32_t *buf;
+    uint32_t size;
+} tensor_dimensions;
+
+#if WASM_ENABLE_WASI_EPHEMERAL_NN != 0
+// sync up with
+// https://github.com/WebAssembly/wasi-nn/blob/71320d95b8c6d43f9af7f44e18b1839db85d89b4/wasi-nn.witx#L19-L28
+// The type of the elements in a tensor.
+typedef enum { fp16 = 0, fp32, fp64, u8, i32, i64 } tensor_type;
+#else
+typedef enum { fp16 = 0, fp32, up8, ip32 } tensor_type;
+#endif /* WASM_ENABLE_WASI_EPHEMERAL_NN != 0 */
+
+// The tensor data.
+//
+// Initially conceived as a sparse representation, each empty cell would be
+// filled with zeros and the array length must match the product of all of the
+// dimensions and the number of bytes in the type (e.g., a 2x2 tensor with
+// 4-byte f32 elements would have a data array of length 16). Naturally, this
+// representation requires some knowledge of how to lay out data in
+// memory--e.g., using row-major ordering--and could perhaps be improved.
+typedef uint8_t *tensor_data;
+
+// A tensor.
+typedef struct {
+    // Describe the size of the tensor (e.g., 2x2x2x2 -> [2, 2, 2, 2]). To
+    // represent a tensor containing a single value, use `[1]` for the tensor
+    // dimensions.
+    tensor_dimensions *dimensions;
+    // Describe the type of element in the tensor (e.g., f32).
+    uint8_t type;
+    uint8_t _pad[3];
+    // Contains the tensor data.
+    tensor_data data;
+} tensor;
+
+/**
+ * GRAPH
+ *
+ */
+
+// The graph initialization data.
+//
+// This consists of an array of buffers because implementing backends may encode
+// their graph IR in parts (e.g., OpenVINO stores its IR and weights
+// separately).
+typedef struct {
+    uint8_t *buf;
+    uint32_t size;
+} graph_builder;
+
+typedef struct {
+    graph_builder *buf;
+    uint32_t size;
+} graph_builder_array;
+
+// An execution graph for performing inference (i.e., a model).
+typedef uint32_t graph;
+
+// sync up with
+// https://github.com/WebAssembly/wasi-nn/blob/main/wit/wasi-nn.wit#L75
+// Describes the encoding of the graph. This allows the API to be implemented by
+// various backends that encode (i.e., serialize) their graph IR with different
+// formats.
+typedef enum {
+    openvino = 0,
+    onnx,
+    tensorflow,
+    pytorch,
+    tensorflowlite,
+    ggml,
+    autodetect,
+    unknown_backend,
+} graph_encoding;
+
+// Define where the graph should be executed.
+typedef enum execution_target { cpu = 0, gpu, tpu } execution_target;
+
+// Bind a `graph` to the input and output tensors for an inference.
+typedef uint32_t graph_execution_context;
+
+/* Definition of 'wasi_nn.h' structs in WASM app format (using offset) */
+
+#ifdef __cplusplus
+}
+#endif
+#endif
